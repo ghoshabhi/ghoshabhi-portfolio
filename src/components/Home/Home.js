6537c9d54b9acaae1a4829e9e19a305cce504efa
Abhishek Ghosh <abhishek.ghosh@copart.com>
2018-08-01 17:48:20 -0700

    Init

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import styles from './styles.css'
+
+import Introduction from '../Introduction';
+
+const Home = () => (
+  <div className="home">
+    <div className="content">
+      <Introduction />
+    </div>
+  </div>
+)
+
+export default Home;
---
+++ src/components/Introduction/Introduction.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles.css';
+
+const Introduction = () => (
+  <div className="introduction">
+    <h1>Hi 👋 I'm Abhishek Ghosh 🐶 </h1>
+  </div>
+)
+
+export default Introduction;
