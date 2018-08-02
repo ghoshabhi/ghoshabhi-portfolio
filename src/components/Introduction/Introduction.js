f7a8abbba7802bca5d055f1e42224dfeba1afb7d
Abhishek Ghosh <abhishek.ghosh@copart.com>
2018-08-01 22:18:01 -0700

    give button the pressing effect

--- src/components/Introduction/Introduction.js
+++ src/components/Introduction/Introduction.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './styles.css';
 
+import Skills from '../Skills';
 import Links from '../Links';
 
 const Introduction = () => (
   <div className="introduction">
     <h1>Hi 👋 I'm Abhishek Ghosh 🐶 </h1>
+    <Skills />
     <Links />
   </div>
 )
