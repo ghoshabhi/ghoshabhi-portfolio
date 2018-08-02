6afb1d45082441bec1516d6d1f631f8da8e98b41
Abhishek Ghosh <abhishek.ghosh@copart.com>
2018-08-01 18:35:28 -0700

    Add online presence links

--- src/components/Introduction/Introduction.js
+++ src/components/Introduction/Introduction.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import styles from './styles.css';
 
+import Links from '../Links';
+
 const Introduction = () => (
   <div className="introduction">
     <h1>Hi 👋 I'm Abhishek Ghosh 🐶 </h1>
+    <Links />
   </div>
 )
 
---
+++ src/components/Links/Links.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import styles from './styles.css'
+
+const LINKS_MAP = {
+  GITHUB: 'https://github.com/ghoshabhi',
+  LINKEDIN: 'https://www.linkedin.com/in/abhishek-ghosh-31280617/',
+  TWITTER: 'https://twitter.com/abhighosh18',
+}
+
+const LinksSeparator = () => (
+  <div className="links_content">
+     <div className="separator" />
+   </div>
+);
+
+const Links = () => (
+ <div className="links_container">
+   <div className="links_content">
+     <a target="_blank" href={LINKS_MAP.GITHUB}>
+      <span className="fab fa-github fa-2x" />
+     </a>
+   </div>
+   <LinksSeparator />
+   <div className="links_content">
+     <a target="_blank" href={LINKS_MAP.LINKEDIN}>
+      <span className="fab fa-linkedin fa-2x" />
+     </a>
+   </div>
+   <LinksSeparator />
+   <div className="links_content">
+     <a target="_blank" href={LINKS_MAP.TWITTER}>
+      <span className="fab fa-twitter fa-2x" />
+     </a>
+   </div>
+ </div>
+)
+
+export default Links;
