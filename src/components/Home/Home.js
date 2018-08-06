920b5228f23c0368eb40710b1747639aa0b04d30
Abhishek Ghosh <abhishek.ghosh@copart.com>
2018-08-06 12:35:05 -0700

    Experiment with Context API

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,15 +4,38 @@ import styles from "./styles.css";
 import Introduction from "../Introduction";
 import JobFlag from "../JobFlag";
 
-const Home = () => (
-  <div className="home">
-    <div className="content">
-      <Introduction />
-      <div className="job-flag__container">
-        <JobFlag />
-      </div>
-    </div>
-  </div>
-);
+import { AppContextProvider } from "../../context";
 
-export default Home;
+export default class Home extends React.Component {
+  state = {
+    isHovered: false
+  };
+
+  render() {
+    return (
+      <AppContextProvider
+        value={{
+          isHovered: this.state.isHovered,
+          onMouseEnter: () =>
+            this.setState({
+              isHovered: true
+            }),
+          onMouseLeave: () =>
+            this.setState({
+              isHovered: false
+            })
+        }}
+      >
+        <div className="home">
+          <div className="content">
+            <Introduction />
+            <div className="job-flag__container">
+              <JobFlag />
+            </div>
+          </div>
+        </div>
+        {this.props.children}
+      </AppContextProvider>
+    );
+  }
+}
--- src/components/JobFlag/JobFlag.js
+++ src/components/JobFlag/JobFlag.js
@@ -1,15 +1,28 @@
 import React, { Component } from "react";
 import styles from "./styles.css";
 
+import { AppContextConsumer } from "../../context";
+
 const JobFlag = () => (
-  <div className="job-flag fade-in">
-    <a href="mailto:abhighosh18@gmail.com?subject=Hello%20Abhishek 👋">
-      <span className="job-flag__text">
-        Hire Me
-        <span className="laptop-icon">👨🏻‍💻</span>
-      </span>
-    </a>
-  </div>
+  <AppContextConsumer>
+    {context => {
+      const className = `job-flag ${context.isHovered ? "fade-in" : ""}`;
+      return (
+        <div
+          className={className}
+          onMouseEnter={context.onMouseEnter}
+          onMouseLeave={context.onMouseLeave}
+        >
+          <a href="mailto:abhighosh18@gmail.com?subject=Hello%20Abhishek 👋">
+            <span className="job-flag__text">
+              Hire Me
+              <span className="laptop-icon">👨🏻‍💻</span>
+            </span>
+          </a>
+        </div>
+      );
+    }}
+  </AppContextConsumer>
 );
 
 export default JobFlag;
